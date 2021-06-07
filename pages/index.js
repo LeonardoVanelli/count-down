67e9bc0a2a3011dbf4e79f6342d71ae21ae37346
Leonardo Vanelli <leonardo.vanelli123@gmail.com>
2021-06-07 01:19:36 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,87 @@
-import Head from 'next/head'
+import CountDown from 'react-countdown'
+import {useState, useEffect} from 'react'
 import styles from '../styles/Home.module.css'
 
+import imageMobile from "../public/castelomobile3.png"
+import imageWeb from "../public/castelobetocarrero.jpg"
+import logo from "../public/logo-footer.png"
+
 export default function Home() {
+  const [date] = useState(new Date(2021, 5, 11, 20))
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);    
+  }, [])
+
+
+  function ImagePath() {
+    return width < 1024 ? imageMobile : imageWeb
+  }
+  
+  const Completionist = () => <span>You are good to go!</span>;
+
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return <div className={styles.cardContainer}>
+          <div className={styles.card}>
+            <div className={styles.date}>
+              {days}
+            </div>
+            <div className= {styles.label}>
+              Dias
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.date}>
+              {hours}
+            </div>
+            <div className= {styles.label}>
+              Horas
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.date}>
+              {minutes}
+            </div>
+            <div className= {styles.label}>
+              Minutos
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.date}>
+              {seconds}
+            </div>
+            <div className= {styles.label}>
+              Segundos
+            </div>
+          </div>
+        </div>;
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+          style={{
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "100%",
+            maxHeight: "100%",        
+            backgroundImage: "url(" + ImagePath() + ")"
+          }}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+          <div className={styles.containerTitle}>
+            <h1 className={styles.title}>Faltam</h1>
+            <CountDown date={date} renderer={renderer} zeroPadDays={2} zeroPadTime={2} className={styles.footer}/>
+            <h1 className={styles.title}>para o</h1>
+            <img src={logo} height="100em"/>
+          </div>
+      </div>
   )
 }
